051b75a3bf5f3fccc6970790ef35ed4dc3ce9819
priyans <priyans05@gmail.com>
2020-02-03 07:13:21 +1100

    Initial Commit

---
+++ loadingDotAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  loadingDotAnimation
+//
+//  Created by PRIYANS on 3/2/20.
+//  Copyright © 2020 PRIYANS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
